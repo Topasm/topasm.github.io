77f7e47c5733aecb8d6d660aaf40bda5b9055a35
Topasm <dhkdwk0215@naver.com>
2026-01-13 11:33:38 +0000

    Deploying to gh-pages from @ Topasm/topasm.github.io@6f890ecaf5d71ac4a7014318e225953d73e84bae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,13 +469,6 @@ ninja.data = [{
           window.open("https://github.com/Topasm", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
